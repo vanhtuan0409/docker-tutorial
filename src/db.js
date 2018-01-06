9969fc87e394f8e489def68c39fc7400add4aa32
Tuấn Vương <vanhtuan0409@gmail.com>
2018-01-06 11:57:53 +0700

    implement app

---
+++ src/db.js
@@ -0,0 +1,23 @@
+const MongoClient = require("mongodb").MongoClient;
+
+let connection = null;
+
+module.exports = {
+  init() {
+    const host = process.env.MONGODB_HOST || "localhost";
+    const port = process.env.MONGODB_PORT || 27017;
+    const database = process.env.MONGODB_DB || "dockertutorial";
+    const dns = `mongodb://${host}:${port}/${database}`;
+    MongoClient.connect(dns, function(err, db) {
+      if (err) {
+        console.error("Cannot connect to mongodb");
+        process.exit(1);
+      }
+      connection = db.db(database);
+      console.log("Connected to mongodb");
+    });
+  },
+  getConnection() {
+    return connection;
+  }
+};
